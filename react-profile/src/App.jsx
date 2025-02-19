bca58a9a3649d862b4ed3cd570725d20497492ef
Emily Hoang <emilyhoanggs@gmail.com>
2025-02-19 12:52:12 -0500

    stuff

--- react-profile/src/App.jsx
+++ react-profile/src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import styles from './App.module.css';
+import NavBar from './components/NavBar/Navbar.jsx';
 
 function App() {
 
   return (
-    <>
-      hi there
-    </>
+    <div className={styles.App}>
+      <NavBar />
+    </div>
   )
 }
 
